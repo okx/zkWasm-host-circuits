119f358d9bc281ebafba697e209329ca54c63740
xingqiang.yuan <xingqiang.yuan@okg.com>
2023-08-07 17:15:31 +0800

    update ut

--- src/host/kvpair.rs
+++ src/host/kvpair.rs
@@ -349,20 +349,31 @@ mod tests {
     use crate::host::merkle::{MerkleNode, MerkleTree};
     use ff::PrimeField;
     use halo2_proofs::pairing::bn256::Fr;
-    use mongodb::bson::doc;
     use std::time::Instant;
 
     #[test]
     fn hash_bench() {
-        let a = [0; 32];
-        let b = [1; 32];
-        MongoMerkle::<20>::hash(&a, &b);
+        let a0 = [0; 32];
+        let a1 = [1; 32];
+        let a2 = [2; 32];
+        let a3 = [3; 32];
+        let a4 = [4; 32];
+        let a5 = [5; 32];
+        let a6 = [6; 32];
+        let a7 = [7; 32];
+        let _hasher = POSEIDON_HASHER.clone();
         let start = Instant::now();
-        for _i in 0..1000 {
+        for _i in 0..40000 {
             let mut hasher = POSEIDON_HASHER.clone();
-            let a = Fr::from_repr(a).unwrap();
-            let b = Fr::from_repr(b).unwrap();
-            hasher.update(&[a, b]);
+            let a0 = Fr::from_repr(a0).unwrap();
+            let a1 = Fr::from_repr(a1).unwrap();
+            let a2 = Fr::from_repr(a2).unwrap();
+            let a3 = Fr::from_repr(a3).unwrap();
+            let a4 = Fr::from_repr(a4).unwrap();
+            let a5 = Fr::from_repr(a5).unwrap();
+            let a6 = Fr::from_repr(a6).unwrap();
+            let a7 = Fr::from_repr(a7).unwrap();
+            hasher.update(&[a0, a1, a2, a3, a4, a5, a6, a7]);
             hasher.squeeze().to_repr();
         }
         println!("cost:{:?}", Instant::now().duration_since(start));
